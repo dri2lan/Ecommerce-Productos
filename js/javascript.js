a25ea50f62386987109ea059a1c6e2c61cd26cf3
dri2lan <soporte.drilan@gmail.com>
2023-08-08 21:32:08 -0400

    modifica html

--- js/javascript.js
+++ js/javascript.js
@@ -17,6 +17,7 @@ if (close) {
 let slideIndex = 0;
 showSlides();
 
+//Testimonial
 function showSlides() {
     const testimonials = document.querySelectorAll('.testimonial');
     
@@ -33,5 +34,38 @@ function showSlides() {
     testimonials[slideIndex - 1].style.display = 'block';
     setTimeout(showSlides, 5000); // Cambiar testimonio cada 5 segundos
 }
+
+
+//Estilo imagen efecto 3D//
+const el = document.getElementById('hero')
+const height = el.clientHeight
+const width = el.clientWidth
+
+el.addEventListener('mousemove', (evt) => {
+    const {layerX, layerY} = evt
+
+    const yRotation = (
+        (layerX - width / 2) / width
+    )   * 20
+
+    const xRotation = (
+        (layerX - height / 2) / height
+    )   * 20
+
+    const string = `
+    perpestive(500px)
+    scale(1.1)
+    rotateX(${xRotation}deg)
+    rotateX(${yRotation}deg)`
+el.style.transform = string    
+})
+
+el.addEventListener('mauseout', () =>{
+    el.style.transform = `
+    perpestive(500px)
+    scale(1)
+    rotateX(0)
+    rotateY(0)`
+})
 
 
